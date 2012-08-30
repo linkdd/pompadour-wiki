4f23d2ee21e84f6ff0a3e178dba5445d3a4b2b15
David Delassus <david.jose.delassus@gmail.com>
2012-08-30 17:12:37 +0200

    Fix urls

--- pompadour_wiki/wiki/views.py
+++ pompadour_wiki/wiki/views.py
@@ -13,16 +13,22 @@ def page(request, wiki):
 
     path = request.path.split('/wiki/{0}/'.format(wiki))[1]
 
+    # Remove trailing slashes
+    while path[-1] == '/':
+        path = path[:-1]
+
     if r.is_dir(path):
         data = {
-            'pages': r.get_tree(path)
+            'pages': r.get_tree(path),
+            'wiki': wiki,
         }
 
         return render_to_response('pages.html', data, context_instance=RequestContext(request))
 
     else:
         data = {
-            'page_content': r.get_content(path)
+            'page_content': r.get_content(path),
+            'wiki': wiki,
         }
 
         return render_to_response('page.html', data, context_instance=RequestContext(request))
