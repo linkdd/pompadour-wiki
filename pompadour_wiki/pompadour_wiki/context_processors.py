20fa71a3359c2abda178461bd1225c98388b9adb
David Delassus <david.jose.delassus@gmail.com>
2012-08-30 14:45:55 +0200

    Add more informations in context_processors.py

--- pompadour_wiki/pompadour_wiki/context_processors.py
+++ pompadour_wiki/pompadour_wiki/context_processors.py
@@ -1,9 +1,25 @@
 from django.core.urlresolvers import reverse
 from django.conf import settings
 
-def nabvar(request):
+
+def _infos():
+    infos = {
+        'title': 'Pompadour Wiki'
+    }
+
+    return infos
+
+def _navbar():
     urls = {'navbar': [
         ('Home', reverse('home')),
     ]}
 
     return urls
+
+def pompadour(request):
+    data = {'pompadour': {}}
+
+    data['pompadour'].update(_infos())
+    data['pompadour'].update(_navbar())
+
+    return data
