f4a1ae2fbed380807d784beadd71bdb60eb1d8b7
David Delassus <david.jose.delassus@gmail.com>
2012-09-07 14:54:07 +0200

    Improve integration of markItUp

--- pompadour_wiki/wiki/forms.py
+++ pompadour_wiki/wiki/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from markitup.widgets import MarkItUpWidget
 
 class EditPageForm(forms.Form):
     path = forms.CharField()
-    content = forms.CharField(widget=forms.Textarea)
+    content = forms.CharField(widget=MarkItUpWidget)
 
